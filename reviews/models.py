34b611a35cbf449bf54b66fea5b2d4d57dc79bd5
Kayla <kajla@MBP-Gulchekhra.Home>
2022-11-12 08:55:44 +0000

    add database

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    name=models.CharField(max_length=25)
+    email=models.EmailField()
+    rating=models.IntegerField()
+    review=models.TextField()
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from reviews.forms import ReviewForm 
+from reviews.models import Review
 
 
 def reviews(request):
+
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
@@ -12,20 +14,9 @@ def reviews(request):
             email = data.get('email')
             review = data.get('review')
             rating = data.get('rating') 
-            with open('data.csv', 'a') as file:
-                file.write(f'{name}|{email}|{review}|{rating}\n') 
-                return redirect('reviews')
-    with open('data.csv') as file:
-        reviews = file.readlines() 
-        if len(reviews) > 0:
-            review_data_1 = reviews[0]
-            name_1 = review_data_1.split('|')[0]
-            email_1 = review_data_1.split('|')[1]
-            review_1 = review_data_1.split('|')[2]
-        else:
-            name_1 = ""
-            email_1 = ""
-            review_1 = "" 
-
+            Review.objects.create(name=name, email=email, review=review, rating=rating)
+            
+    
     form = ReviewForm()
-    return render(request, 'reviews.html', {'form': form, 'name_1': name_1, 'email_1': email_1, 'review_1': review_1})
+    reviews=Review.objects.all()
+    return render(request, 'reviews.html', {'form': form, 'reviews': reviews})
